53b524b3c88dda8e3dc86f4b55413e3e42be28c3
Vahid Al <thevahidal@gmail.com>
2022-02-22 03:21:28 +0330

    handled access token expiring

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { login } from '../actions/auth';
 
 import { API_BASE_URL } from '../constants/api';
+import { store } from '../store';
+import { refreshTokenAPI } from './auth';
 
 const fetchAPI = axios.create({
   baseURL: API_BASE_URL,
@@ -15,7 +18,7 @@ fetchAPI.defaults.headers['Authorization'] = 'Basic '
 
 fetchAPI.interceptors.request.use(
   config => {
-    const token = localStorage.getItem('token');
+    const token = store.getState().user.access;
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
@@ -26,5 +29,48 @@ fetchAPI.interceptors.request.use(
     return config;
   },
 );
+
+
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+fetchAPI.interceptors.response.use(response => {
+  return response;
+}, error => {
+  const { config, response: { status } } = error;
+  const originalRequest = config;
+
+  if (status === 401) {
+    if (!isRefreshing) {
+      isRefreshing = true;
+      refreshTokenAPI()
+        .then(res => {
+          const { access, refresh } = res.data;
+          store.dispatch(login(access, refresh));
+          isRefreshing = false;
+          onRefreshed(access);
+        });
+    }
+
+    const retryOriginalRequest = new Promise((resolve, reject) => {
+      subscribeRefreshToken(token => {
+        originalRequest.headers['Authorization'] = 'Bearer ' + token;
+        resolve(axios(originalRequest));
+      });
+    });
+    return retryOriginalRequest;
+  } else {
+    return Promise.reject(error);
+  }
+});
+
+
+const subscribeRefreshToken = (cb) => {
+  refreshSubscribers.push(cb);
+}
+
+const onRefreshed = (token) => {
+  refreshSubscribers.map(cb => cb(token));
+}
 
 export default fetchAPI;
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,27 +1,56 @@
+import { useEffect } from "react";
 import { Navbar as RBNavbar, Nav, Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import useSetState from "react-use-setstate";
 
 import { Heading } from '../typography'
+import { organizationsAPI } from '../../apis/users'
 
 import * as styles from './Navbar.styled'
 
 const Navbar = props => {
+    const [state, setState] = useSetState({
+    })
+
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated)
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            organizationsAPI().then(res => {
+                console.log(res)
+            })
+        }
+    }, [isAuthenticated])
 
     return (<styles.Wrapper>
         <RBNavbar>
             <Container className='py-2'>
-                <RBNavbar.Brand className='d-flex align-items-center'>
-                    <img
-                        alt="brand logo"
-                        src="/logo192.png"
-                        width="30"
-                        height="30"
-                    />{' '}
-                    <Heading variant={6} className='mx-2'>Hoopoe</Heading>
-                </RBNavbar.Brand>
+                <div className="d-flex align-items-center">
+                    <RBNavbar.Brand className='d-flex align-items-center'>
+                        <img
+                            alt="brand logo"
+                            src="/logo192.png"
+                            width="30"
+                            height="30"
+                        />{' '}
+                        <Heading variant={6} className='mx-2'>Hoopoe</Heading>
+                    </RBNavbar.Brand>
+                    {'|'}
+                </div>
                 <RBNavbar.Toggle />
-                <RBNavbar.Collapse>
+                <RBNavbar.Collapse className='d-flex justify-content-end'>
                     <Nav>
-                        {/* <Nav.Link>Link</Nav.Link> */}
+                        {/* <Nav.Link href='/'>Home</Nav.Link> */}
+                        {
+                            !isAuthenticated
+                                ? <>
+                                    <Nav.Link href='/auth/login'>Login</Nav.Link>
+                                    <Nav.Link href='/auth/register'>Register</Nav.Link>
+                                </>
+                                : <>
+                                    <Nav.Link href='/profile'>Profile</Nav.Link>
+                                </>
+                        }
                     </Nav>
                 </RBNavbar.Collapse>
             </Container>
